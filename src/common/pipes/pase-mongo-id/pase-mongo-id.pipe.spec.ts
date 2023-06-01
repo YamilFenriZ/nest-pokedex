89f8ae7913fc71e72ee6530de6f6f6cbc3edf27d
Yhamil FenrZ <yamilfenriz@gmail.com>
2023-05-31 22:52:54 -0400

    Antes de insertar por lote

--- src/common/pipes/pase-mongo-id/pase-mongo-id.pipe.spec.ts
+++
@@ -1,7 +0,0 @@
-import { PaseMongoIdPipe } from './pase-mongo-id.pipe';
-
-describe('PaseMongoIdPipe', () => {
-  it('should be defined', () => {
-    expect(new PaseMongoIdPipe()).toBeDefined();
-  });
-});
