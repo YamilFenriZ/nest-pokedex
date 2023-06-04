394f5b90bb10148aacaf5f8a3c20730ed30afbf6
Yhamil FenrZ <yamilfenriz@gmail.com>
2023-06-03 20:38:34 -0400

    Seccion - con variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,24 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration } from './config/env.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 
 @Module({
   imports: [
+
+    ConfigModule.forRoot({
+      load: [ EnvConfiguration ],
+      validationSchema: JoiValidationSchema
+    }),
     
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..','public')
     }),
 
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot( process.env.MONGODB ),
 
     PokemonModule,
 
@@ -24,4 +32,9 @@ import { SeedModule } from './seed/seed.module';
 
   ],  
 })
-export class AppModule {}
+export class AppModule {
+  // constructor(){
+  //   console.log( process.env );
+    
+  // }
+}
